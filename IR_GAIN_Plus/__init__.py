10e20e6199aced7e98f603cffd4bae2b9596175e
Hanwen-Hu <hanwen_hu@126.com>
2023-11-20 15:08:25 +0800

    IR-Net-Plus

--- IR_GAIN_Plus/__init__.py
+++ IR_GAIN_Plus/__init__.py
@@ -13,6 +13,22 @@ class EXE:
         self.dataset = self.dict[args.dataset](args.length, 1, args.device, args.miss_rate)
         self.criterion = torch.nn.MSELoss()
 
+    def generator_loss(self, x, m):
+        x[m == 0] = 0
+        x_1 = self.generator(x, m)
+        x_impute = x_1.clone()
+        x_impute[m == 1] = x[m == 1]
+        p = self.discriminator(x_impute, m)
+        loss_g = 3 * self.criterion(x_1[m == 1], x[m == 1]) - self.criterion(p, m)
+        x_2 = self.generator(x_1, 1 - m)
+        return loss_g + 2 * self.criterion(x_2[m == 1], x[m == 1])
+
+    def discriminator_loss(self, x, m):
+        x_impute = self.generator(x, m)
+        x_impute[m == 1] = x[m == 1]
+        p = self.discriminator(x_impute, m)
+        return self.criterion(p, m), x_impute
+
     def run(self):
         mse_loss = torch.nn.MSELoss()
         mae_loss = lambda a, b: torch.mean(torch.abs(a - b))
@@ -23,29 +39,19 @@ class EXE:
             mse_error, mae_error, batch_num = 0, 0, 0
             for i, (x, y, m) in enumerate(loader):
                 # Training
-                x[m==0] = 0
                 optimizer_g.zero_grad()
-                x_1 = self.generator(x, m)
-                x_1_ = x_1.clone()
-                x_1_[m==1] = x[m==1]
-                p = self.discriminator(x_1_, m)
-                loss_g = 3 * mse_loss(out[m==1], x[m==1]) - mse_loss(p, m)
-                x_tilde = self.generator(out, 1 - m)
-                loss_g = loss_g + 2 * mse_loss(x_tilde[m==1], x[m==1])
+                loss_g = self.generator_loss(x, m)
                 loss_g.backward()
                 optimizer_g.step()
 
-                # Testing
                 optimizer_d.zero_grad()
-                out = self.generator(x, m)
-                out[m==1] = x[m==1]
-                p = self.discriminator(out, m)
-                loss_d = mse_loss(p, m)
+                loss_d, result = self.discriminator_loss(x, m)
                 loss_d.backward()
                 optimizer_d.step()
 
-                mse_error += mse_loss(out[m==0], y[m==0]).item()
-                mae_error += mae_loss(out[m==0], y[m==0]).item()
+                # Testing
+                mse_error += mse_loss(result[m==0], y[m==0]).item()
+                mae_error += mae_loss(result[m==0], y[m==0]).item()
                 batch_num += 1
             print(epoch, round(mse_error/ batch_num, 4), round(mae_error / batch_num, 4))
             torch.save(self.generator, 'Files/IRGAINPlus_' + self.args.dataset + '_' + str(int(self.args.r_miss * 10)) + '.pth')
